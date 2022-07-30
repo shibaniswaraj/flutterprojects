9c3956136289061b0588f3d0691ea9d31d17fe48
shivani swaraj <88shivaniswaraj@gmail.com>
2022-07-30 21:50:11 +0530

    layout challenge

---
+++ micard_v2/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                height: double.infinity,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.yellowAccent,
+                    height: 100.0,
+                    width: 100.0,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 100.0,
+                    width: 100.0,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                height: double.infinity,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
